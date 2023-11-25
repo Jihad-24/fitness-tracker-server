6e0d3e8f5699f079e6cfa94fcd952729e4faa070
Jihad Hasan <mdjihat0021@gmail.com>
2023-11-25 11:17:19 +0600

    added user api

--- index.js
+++ index.js
@@ -30,8 +30,74 @@ async function run() {
     // await client.connect();
 
     const userCollection = client.db('FitnessTrackerDB').collection('users');
+    const subscribeCollection = client.db('FitnessTrackerDB').collection('subscribes');
 
-    
+    // subscribe api
+    app.post('/subscribes',async(req,res)=>{
+        const data = req.body;
+        const result= await subscribeCollection.insertOne(data);
+        res.send(result);
+    })
+
+    // user api
+    app.get('/users', async (req, res) => {
+        const result = await userCollection.find().toArray();
+        res.send(result);
+      })
+
+      app.get('/users/:email', async (req, res) => {
+        const email = req.params.email;
+        const user = await userCollection.findOne({ email: email });
+        res.send(user);
+    });
+  
+      app.get('/users/admin/:email', async (req, res) => {
+        const email = req?.params?.email;
+        if (email !== req.decoded.email) {
+          return res.status(403).send({ message: 'forbiddien access' })
+        }
+  
+        const query = { email: email };
+        const user = await userCollection.findOne(query);
+        let admin = false;
+        if (user) {
+          admin = user?.role === 'admin';
+        }
+        res.send({ admin });
+      })
+  
+      app.post('/users', async (req, res) => {
+        const user = req.body;
+        // insert email if user doesen't exist
+        // you can do this many ways (1. email unique, 2. upsert, 3. simple cheaking)
+        const query = { email: user.email };
+        const existingUser = await userCollection.findOne(query);
+        if (existingUser) {
+          return res.send({ message: 'user already exist', insertedId: null })
+        }
+  
+        const result = await userCollection.insertOne(user);
+        res.send(result);
+      })
+  
+      app.patch('/users/admin/:id', async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const updateDoc = {
+          $set: {
+            role: 'admin'
+          }
+        }
+        const result = await userCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      })
+  
+      app.delete('/users/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await userCollection.deleteOne(query);
+        res.send(result);
+      })
 
 
 
