5f80c8524ab7b91a900778e9302fb9c273515d2c
Jihad Hasan <mdjihat0021@gmail.com>
2023-11-28 00:27:35 +0600

    finished jwt

--- index.js
+++ index.js
@@ -39,6 +39,43 @@ async function run() {
     const classCollection = client.db('FitnessTrackerDB').collection('classes');
     const teamCollection = client.db('FitnessTrackerDB').collection('teams');
 
+
+      // jwt token api
+      app.post('/jwt', async (req, res) => {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
+        res.send({ token });
+      })
+  
+      // middlewares
+      const verifyToken = (req, res, next) => {
+        // console.log("inside verify token", req.headers.authorization);
+        if (!req.headers.authorization) {
+          return res.status(401).send({ message: 'unauthorized access' })
+        }
+        const token = req.headers.authorization.split(' ')[1];
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+          if (err) {
+            return res.status(401).send({ message: 'unauthorized access' })
+          }
+          req.decoded = decoded;
+          next();
+        })
+      }
+  
+      // use verify admin after verifytoken
+      const verifyAdmin = async (req, res, next) => {
+        const email = req.decoded.email;
+        const query = { email: email };
+        const user = await userCollection.findOne(query);
+        const isAdmin = user?.role === 'admin';
+        if (!isAdmin) {
+          return res.status(403).send({ message: 'forbiddien access' })
+        }
+        next();
+      }
+
+
     // teams api
     app.get('/teams', async (req, res) => {
       const result = await teamCollection.find().toArray();
@@ -50,7 +87,7 @@ async function run() {
       res.send(result);
     })
 
-    app.post('/classes', async (req, res) => {
+    app.post('/classes',verifyToken,  async (req, res) => {
       const item = req.body;
       const result = await classCollection.insertOne(item);
       res.send(result);
@@ -62,7 +99,7 @@ async function run() {
       res.send(result);
     })
    
-    app.post('/forums', async (req, res) => {
+    app.post('/forums',verifyToken,  async (req, res) => {
       const item = req.body;
       const result = await forumCollection.insertOne(item);
       res.send(result);
@@ -75,7 +112,7 @@ async function run() {
                 { _id: new ObjectId(forumId) },
                 { $inc: { upVotes: 1 } }
             );
-            res.send('Up-vote successful');
+            res.send(result);
         } catch (error) {
             console.error('Error up-voting:', error);
             res.status(500).send('Error up-voting');
@@ -89,7 +126,7 @@ async function run() {
                 { _id: new ObjectId(forumId) },
                 { $inc: { downVotes: 1 } }
             );
-            res.send('Down-vote successful');
+            res.send(result);
         } catch (error) {
             console.error('Error down-voting:', error);
             res.status(500).send('Error down-voting');
@@ -100,10 +137,7 @@ async function run() {
       const forumId = req.params.id;
       try {
         const result = await forumCollection.findOne({ _id: new ObjectId(forumId) });
-        if (!result) {
-          res.status(404).send('Forum post not found');
-          return;
-        }
+
         res.send(result);
       } catch (error) {
         console.error('Error fetching forum post:', error);
@@ -111,19 +145,31 @@ async function run() {
       }
     });
     
-
-    // trainer api
+// teacher api
     app.get('/teacher', async (req, res) => {
       const result = await teacherCollection.find().toArray();
       res.send(result);
     })
 
+    app.put('/teacher/:id',async(req,res)=>{
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          status: 'paid'
+        }
+      };
+        const result = await teacherCollection.updateOne(filter, updateDoc);
+        res.send(result)
+    })
+
+    // trainer api
     app.get('/trainers', async (req, res) => {
       const result = await trainerCollection.find().toArray();
       res.send(result);
     })
 
-    app.post('/trainers', async (req, res) => {
+    app.post('/trainers',verifyToken, async (req, res) => {
       const data = req.body;
       const result = await trainerCollection.insertOne(data);
       res.send(result);
@@ -134,7 +180,7 @@ async function run() {
       const result = await planCollection.find().toArray()
       res.send(result);
     })
-    app.post('/plans', async (req, res) => {
+    app.post('/plans',verifyToken, async (req, res) => {
       const data = req.body;
       const result = await planCollection.insertOne(data)
       res.send(result);
@@ -153,13 +199,13 @@ async function run() {
     })
 
     // user api
-    app.get('/users', async (req, res) => {
+    app.get('/users',verifyToken,  async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
     })
     app.get('/users/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
+      const query = {_id: id}
       const result = await userCollection.findOne(query)
       res.send(result);
     })
@@ -170,19 +216,40 @@ async function run() {
       res.send(user);
     });
 
-    app.get('/users/admin/:email', async (req, res) => {
+    app.get('/users/admin/:email',verifyToken, async (req, res) => {
       const email = req?.params?.email;
+      // console.log(email);
+      // console.log(req.decoded.email);
       if (email !== req.decoded.email) {
         return res.status(403).send({ message: 'forbiddien access' })
       }
-
       const query = { email: email };
+      console.log(query);
       const user = await userCollection.findOne(query);
+      // console.log(user);
       let admin = false;
       if (user) {
         admin = user?.role === 'admin';
       }
       res.send({ admin });
+    })
+
+    app.get('/users/trainer/:email',verifyToken, async (req, res) => {
+      const email = req?.params?.email;
+      // console.log(email);
+      // console.log(req.decoded.email);
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbiddien access' })
+      }
+      const query = { email: email };
+      // console.log(query);
+      const user = await userCollection.findOne(query);
+      // console.log(user);
+      let trainer = false;
+      if (user) {
+        trainer = user?.role === 'trainer';
+      }
+      res.send({ trainer });
     })
 
     app.post('/users', async (req, res) => {
@@ -214,7 +281,7 @@ async function run() {
       }
     });
 
-    app.patch('/users/:email', async (req, res) => {
+    app.patch('/users/:email',verifyToken, verifyAdmin, async (req, res) => {
       const userEmail = req.params.email;
       const filter = { email: userEmail };
       const updateDoc = {
@@ -238,38 +305,6 @@ async function run() {
       }
     });
 
-    // app.patch('/users/:id', async (req, res) => {
-    //   const id = req.params.id;
-    //   const filter = { _id: new ObjectId(id) };
-    //   const updateDoc = {
-    //     $set: {
-    //       role: 'trainer'
-    //     }
-    //   }
-    //   const result = await userCollection.updateOne(filter, updateDoc);
-    //   res.send(result);
-    // })
-
-    // app.patch('/users/admin/:id', async (req, res) => {
-    //   const id = req.params.id;
-    //   const filter = { _id: new ObjectId(id) };
-    //   const updateDoc = {
-    //     $set: {
-    //       role: 'admin'
-    //     }
-    //   }
-    //   const result = await userCollection.updateOne(filter, updateDoc);
-    //   res.send(result);
-    // })
-
-    app.delete('/users/:id', async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await userCollection.deleteOne(query);
-      res.send(result);
-    })
-
-
     // payment intent api
     app.post('/create-payment-intent', async (req, res) => {
       const { price } = req.body;
@@ -286,11 +321,16 @@ async function run() {
       })
     })
 
-    app.get('/payments/:email', async (req, res) => {
+    app.get('/payments/:email',verifyToken, async (req, res) => {
       const email = req.params.email;
       const user = await paymentCollection.findOne({ email: email });
       res.send(user);
     });
+
+    app.get('/payments',async(req,res)=>{
+      const result = await paymentCollection.find().toArray();
+      res.send(result);
+    })
 
     app.post('/payments', async (req, res) => {
       const payment = req.body;
